c279e799a3541374bdb2ad91bd075e8aedeaacb8
leandrodeleon22 <leandrodeleon22@gmail.com>
2024-02-24 21:29:17 -0500

    put data to app component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+// const sampleDataForPhotoListItem = {
+//   id: "1",
+//   location: {
+//     city: "Montreal",
+//     country: "Canada",
+//   },
+//   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+//   username: "Joe Example",
+//   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+// };s
 
-const PhotoListItem = () => {
+const PhotoListItem = ({ sampleDataForPhotoListItem }) => {
   /* Insert React */
   return (
     <>
