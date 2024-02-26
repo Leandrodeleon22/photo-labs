99693bb022a0b8c5d214379442a440ac62075b3b
leandrodeleon22 <leandrodeleon22@gmail.com>
2024-02-25 20:26:57 -0500

    add homeRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import "./App.scss";
 import PhotoList from "components/PhotoList";
 import TopicList from "components/TopicList";
 import TopNavigation from "components/TopNavigationBar";
+import HomeRoute from "routes/HomeRoute";
 
 // Note: Rendering a single component to build components in isolation
 // const sampleDataForPhotoListItem = {
@@ -27,9 +28,7 @@ import TopNavigation from "components/TopNavigationBar";
 const App = () => {
   return (
     <div className="App">
-      <TopNavigation />
-
-      <PhotoList />
+      <HomeRoute />
     </div>
   );
 };
