eccdad42323c5dd884780902453188a1a5bf5c75
leandrodeleon22 <leandrodeleon22@gmail.com>
2024-03-03 08:14:10 -0500

    useReducer for multiple state

--- frontend/src/hooks/useApplicationData.jsx
+++ frontend/src/hooks/useApplicationData.jsx
@@ -1,21 +1,42 @@
-import React from "react";
-import { useState } from "react";
+import React, { useReducer } from "react";
+import photos from "mocks/photos";
+const initialState = {
+  favorites: [],
+  activePhoto: null,
+  showModal: false,
+  isLike: false,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_FAVORITES":
+      return { ...state, favorites: action.payload };
+    case "SET_ACTIVE_PHOTO":
+      return { ...state, activePhoto: action.payload };
+    case "SET_SHOW_MODAL":
+      return { ...state, showModal: action.payload };
+    case "SET_IS_LIKE":
+      return { ...state, isLike: action.payload };
+    default:
+      return state;
+  }
+};
 
 const useApplicationData = (photos) => {
-  const [favorites, setFavorites] = useState(photos);
-  const [activePhoto, setActivePhoto] = useState(null);
-  const [showModal, setShowModal] = useState(false);
-  const [isLike, setIsLike] = useState(false);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return {
-    favorites,
-    setFavorites,
-    activePhoto,
-    setActivePhoto,
-    showModal,
-    setShowModal,
-    isLike,
-    setIsLike,
+    favorites: state.favorites && photos,
+    setFavorites: (favorites) =>
+      dispatch({ type: "SET_FAVORITES", payload: favorites }),
+    activePhoto: state.activePhoto,
+    setActivePhoto: (activePhoto) =>
+      dispatch({ type: "SET_ACTIVE_PHOTO", payload: activePhoto }),
+    showModal: state.showModal,
+    setShowModal: (showModal) =>
+      dispatch({ type: "SET_SHOW_MODAL", payload: showModal }),
+    isLike: state.isLike,
+    setIsLike: (isLike) => dispatch({ type: "SET_IS_LIKE", payload: isLike }),
   };
 };
 
